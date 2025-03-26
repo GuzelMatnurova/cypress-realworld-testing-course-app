cdb789ae794750c987c7e691166fd945261cadec
Guzel <matnurova@yandex.ru>
2025-03-26 12:59:30 +0100

    add  e2e tests

---
+++ cypress.config.ts
@@ -0,0 +1,10 @@
+import { defineConfig } from "cypress";
+
+export default defineConfig({
+  projectId: '1g1bgq',
+  e2e: {
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+  },
+});
