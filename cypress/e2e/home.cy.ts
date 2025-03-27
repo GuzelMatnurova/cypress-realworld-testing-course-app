ed6c2377795c5b642e7a8661937867afb24e79de
Guzel <matnurova@yandex.ru>
2025-03-27 13:51:50 +0100

    Changes in workflow

--- cypress/e2e/home.cy.ts
+++ cypress/e2e/home.cy.ts
@@ -4,13 +4,19 @@ describe('home page', () => {
     cy.viewport(1600, 800)
   })
   context("Hero section", () => {
-  it('the h1 contains the correct text', () => {
-    cy.getByData("hero-heading").contains("Testing Next.js Applications with Cypress")
+    it('the h1 contains the correct text', () => {
+      cy.getByData("hero-heading").contains("Testing Next.js Applications with Cypress")
+    })
+    it("the features on the homepage are correct", () => {
+      cy.get("dt").eq(0).contains("4 Courses")
+      cy.get("dt").eq(1).contains("25+ Lessons")
+      cy.get("dt").eq(2).contains("Free and Open Source")
+    })
   })
-  it("the features on the homepage are correct", () => {
-    cy.get("dt").eq(0).contains("4 Courses")
-    cy.get("dt").eq(1).contains("25+ Lessons")
-    cy.get("dt").eq(2).contains("Free and Open Source")
-  })
+  context("Courses section", () => {
+    it("Course: Testing Your First Next.js Application", () => {
+      cy.getByData("course-0").find("a").contains("Get started").click()
+      
+    })
   })
 })
